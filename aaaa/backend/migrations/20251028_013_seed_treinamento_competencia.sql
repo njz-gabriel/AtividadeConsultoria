0dfc06062a4087e128013a46fd363607a60942e3
GIOVANNE ISAAC MARQUES <25170154@educ123.sp.senai.br>
2026-02-09 09:50:19 -0300

    ExcluindoAAAA

--- aaaa/backend/migrations/20251028_013_seed_treinamento_competencia.sql
+++
@@ -1,7 +0,0 @@
--- D) Ligar Cursos às Competências (Exemplos)
--- Curso 1 (Agil) -> Agilidade(1), Gestão(2)
-INSERT INTO treinamento_competencia (treinamento_id, competencia_id) VALUES (1, 1), (1, 2);
--- Curso 2 (Segurança Info) -> Tecnologia(3)
-INSERT INTO treinamento_competencia (treinamento_id, competencia_id) VALUES (2, 3);
--- Curso 3 (Alta Tensão) -> Segurança(7), Tecnologia(3)
-INSERT INTO treinamento_competencia (treinamento_id, competencia_id) VALUES (3, 7), (3, 3);
--- aaaa/docs/docs.md
+++
@@ -1,1 +0,0 @@
-Desenvolva sua documentação aqui
