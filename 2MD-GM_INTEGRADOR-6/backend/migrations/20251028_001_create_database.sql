89a9f1e61aac8d21c9bac9b1ed98d2f99e741de5
GIOVANNE ISAAC MARQUES <25170154@educ123.sp.senai.br>
2026-02-09 10:08:21 -0300

    Renomeando Migrations

--- 2MD-GM_INTEGRADOR-6/backend/migrations/20251028_001_create_database.sql
+++ 2MD-GM_INTEGRADOR-6/backend/migrations/20251028_001_create_database.sql
@@ -2,6 +2,7 @@
 -- Data: 2025-01-15
 -- Descrição: Criação inicial do banco de dados
 
+DROP DATABASE IF EXISTS produtos_api;
 CREATE DATABASE IF NOT EXISTS produtos_api;
 
 -- Usar o banco de dados criado
